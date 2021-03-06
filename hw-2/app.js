dd8138ae186703e8556483e16e13fe1d6f0e3c47
Andrew Krupin <andrew.krupin@empeek.com>
2021-03-06 15:54:04 +0200

    base search logic implemented

--- hw-2/app.js
+++ hw-2/app.js
@@ -2,29 +2,34 @@ const FindLib = require('./lib_ee');
 
 const MyEE = new FindLib();
 
-const dirToFind = process.argv[2] || __dirname;
+const dirToSearch = process.argv[2] || __dirname;
 
 MyEE.on('started', () => {
-    console.log('Parse started');
+  console.log('Parse started');
 });
 
-MyEE.emit('start', dirToFind, '.*e\.json');
+MyEE.emit('start', dirToSearch, '.*e\.json');
 
-MyEE.on('find', ({ path, name }) => {
-    // path относительно dirToFind
-    // name - названия файла
+MyEE.on('find', ({ name, path }) => {
+  console.log(`File ${name} found in path: ${path}`);
+  // path относительно dirToSearch
+  // name - названия файла
 });
 
 // progress эмититься каждые 3 секунд, если не было события find
 MyEE.on('progress', ({files, dir}) => {
-    console.log(`Progress: ${files} files, ${dir} dir`);
+  console.log(`Progress: ${files} files, ${dir} dir`);
 });
 
 MyEE.once('complete', ({ scanned: { files, dirs }, found }) => {
-    console.log("complete", files, dirs, found);
-    // found => [{ name, path }] path относительно dirToFind
+  console.log(
+    "complete",
+    `${files} files`,
+    `${dirs} dirs`,
+    found
+  );
 });
 
 MyEE.once('error', (err) => {
-    process.exit(1);
+  process.exit(1);
 });
--- hw-2/lib_ee.js
+++ hw-2/lib_ee.js
@@ -1,12 +1,34 @@
+const fs = require('fs');
+const { join } = require('path');
+
 const EventEmitter = require("events");
 
+class FoundFile {
+  constructor( name, path ) {
+    this.name = name;
+    this.path = path;
+  }
+}
+
 class FindLib extends EventEmitter {
+  dirs = 0;
+  files;
+  result;
+  baseDirToSearch;
+
   constructor() {
     super();
 
-    this.on("start", (...args) => {
-      console.log("start");
-      this.parseDir(...args);
+    this.on("start", (dirToSearch, fileName) => {
+      this.baseDirToSearch = dirToSearch;
+      this.recParseDir(dirToSearch, fileName);
+
+      process.nextTick(() => {
+        this.emit("complete", {
+          scanned: { files: this.result.length, dirs: this.dirs },
+          found: this.result,
+        });
+      });
     });
 
     setTimeout(() => {
@@ -14,15 +36,40 @@ class FindLib extends EventEmitter {
     }, 0);
   }
 
-  parseDir() {
-    console.log("parse");
+  recParseDir(dirToSearch, fileName, readFiles, interimResult) {
+    this.files = readFiles || fs.readdirSync(dirToSearch, { withFileTypes: true });
+    this.result = interimResult || [];
 
-    setTimeout(() => {
-      this.emit("complete", {
-        scanned: { files: 1, dirs: 1 },
-        found: [],
-      });
-    }, 0);
+    this.files.forEach(file => {
+
+      const modifiedPath = join(dirToSearch, file.name);
+
+      if (file.isDirectory()) {
+        this.dirs++;
+        this.result = this.recParseDir(
+          modifiedPath,
+          fileName,
+          fs.readdirSync(modifiedPath, { withFileTypes: true }),
+          this.result
+        );
+      } else {
+        if (file.name.match(fileName)) {
+          const foundFile = new FoundFile(file.name, this.getRelativePath(modifiedPath));
+
+          process.nextTick(() => {
+            this.emit("find", foundFile);
+          });
+
+          this.result.push(foundFile);
+        }
+      }
+    });
+
+    return this.result;
+  }
+
+  getRelativePath(absPath) {
+    return absPath.split(this.baseDirToSearch)[1];
   }
 }
 
